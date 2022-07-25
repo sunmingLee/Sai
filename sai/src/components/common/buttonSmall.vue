36ce933dc5530c06e9b13b4663d8ec5ef4f6ca7d
LeeSunMin <julia9803@naver.com>
2022-07-25 13:50:53 +0900

    Feat: Add common - inputBox, buttonSmall

---
+++ sai/src/components/common/buttonSmall.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <button :class="buttonUsage" @click="onLogin">{{ buttonText }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "buttonSmall",
+  props: {
+    buttonUsage: String,
+    buttonText: String,
+  },
+
+  methods: {
+    onLogin() {
+      alert(this.buttonText);
+    },
+  },
+};
+</script>
+
+<style>
+.positive {
+  background-color: #7b371c;
+  width: 80px;
+  height: 30px;
+  border-radius: 10px;
+  border: none;
+  color: white;
+}
+</style>
---
+++ sai/src/components/common/inputBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label for="inputBox" v-if="hasLabel">{{ labelName }}</label>
+    <input id="inputBox" type="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inputBox",
+  props: {
+    hasLabel: Boolean,
+    labelName: String,
+  },
+};
+</script>
+
+<style>
+#inputBox {
+  margin-left: 5px;
+  margin-bottom: 10px;
+  width: 190px;
+  border-radius: 5px;
+  border: 1px solid #ae5f40;
+}
+</style>
