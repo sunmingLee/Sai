60de24c772a5dc9014d591d91a4b98394f279aa0
Jin Sang Woo <hazq_@naver.com>
2022-07-25 12:52:39 +0900

    Feat: "User Dto" User Dto 코드 작성 중 테이블명, 컬럼명 일치 확인 위해 데이터베이스 스키마 생성 필요

--- sai/src/main/java/com/sai/dto/UserDto.java
+++ sai/src/main/java/com/sai/dto/UserDto.java
@@ -1,5 +1,37 @@
 package com.sai.dto;
 
-public class UserDto {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class UserDto {
+	
+	@Id
+	private String user_id;
+	
+	private String family_code;
+	
+	private String name;
+	
+	private String email;
+	
+	private String password;
+	
+	private String nickname;
+	
+	private Date birthday;
+	
+	private boolean lunar;
+	
+	private String user_image;
+	
+	private String user_message;
+	
 }
