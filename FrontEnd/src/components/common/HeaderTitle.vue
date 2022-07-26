d4af796be206d99c7a2dee38112e79f26b628f2e
LeeSunMin <julia9803@naver.com>
2022-07-26 16:40:17 +0900

    Cherry-pick commit "c05e6aac"

--- FrontEnd/src/components/common/HeaderTitle.vue
+++ FrontEnd/src/components/common/HeaderTitle.vue
@@ -1,13 +1,49 @@
 <template>
-    <div>
-        <h2>{{ title }}</h2>
-    </div>
+  <div class="header">
+      <img :class="visibilityBack" src="@/assets/images/arrow-left.png" alt="arrow-left" >
+      <h2>{{ title }}</h2>
+      <img :class="visibilityIcon" src="@/assets/images/calendar-month.png" alt="calendar">
+  </div>
 </template>
 <script>
 export default {
   name: 'HeaderTitle',
   props: {
-    title: String
+    title: String,
+    hasBack: Boolean,
+    hasIcon: Boolean
+  },
+  data () {
+    return {
+      visibilityBack: '',
+      visibilityIcon: ''
+    }
+  },
+  created () {
+    if (!this.hasBack) {
+      this.visibilityBack = 'hidden'
+    }
+    if (!this.hasIcon) {
+      this.visibilityIcon = 'hidden'
+    }
   }
 }
 </script>
+
+<style>
+h2{
+  color: #7B371C;
+}
+.header{
+  position: sticky;
+  top: 0;
+  padding-left: 5px;
+  padding-right: 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.hidden{
+  visibility: hidden;
+}
+</style>
