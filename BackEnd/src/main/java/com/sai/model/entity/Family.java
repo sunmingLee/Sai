24d848ba200bd94ff301063fc6d261eee1e27b27
KimHeeYoung <ahwl8240@naver.com>
2022-07-27 07:32:05 +0900

    feat: family

---
+++ BackEnd/src/main/java/com/sai/model/entity/Family.java
@@ -0,0 +1,18 @@
+//package com.sai.model.entity;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@NoArgsConstructor
+//@Entity
+//public class Family {
+//	
+////	@Id
+////	@GeneratedValue(strategy = GenerationType.AUTO)
+//}
