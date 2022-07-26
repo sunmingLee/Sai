6a911701ac71f7789b7a8082f2c5db19c31971f9
Jin Sang Woo <hazq_@naver.com>
2022-07-26 11:10:29 +0900

    Fixed: "JPA 설정 일부 변경" generate-ddl=false hibernate.ddl-auto=none

--- sai/src/main/java/com/sai/dto/UserDto.java
+++ sai/src/main/java/com/sai/dto/UserDto.java
@@ -3,6 +3,8 @@ package com.sai.dto;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Getter;
@@ -14,24 +16,40 @@ import lombok.NoArgsConstructor;
 public class UserDto {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String user_id;
 	
-	private String family_code;
+	// 가족 ID
+	private String family_id;
 	
-	private String name;
+	// 유저 이름
+	private String user_name;
 	
+	// 유저 이메일
 	private String email;
 	
+	// 패스워드
 	private String password;
 	
+	// 유저 닉네임
 	private String nickname;
 	
+	// 유저 생일
 	private Date birthday;
 	
+	// 음력 사용 여부
 	private boolean lunar;
 	
-	private String user_image;
+	// 프로필 사진 이미지 경로
+	private String user_image_path;
+
+	// 프로필 사진 이미지 이름
+	private String user_image_name;
 	
+	// 프로필 사진 이미지 속성
+	private String user_image_type;
+	
+	// 프로필 메세지
 	private String user_message;
 	
 }
