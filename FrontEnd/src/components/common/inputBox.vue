a88608c79c649fa4c2e07550f77187daeb8a2aaa
LeeSunMin <julia9803@naver.com>
2022-07-27 14:23:46 +0900

    Design: Add conditions

--- FrontEnd/src/components/common/inputBox.vue
+++ FrontEnd/src/components/common/inputBox.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <label class="label-box" for="input-box" v-if="hasLabel">{{ labelName }}</label>
-    <input class="input-box" id="input-box" type="text" @keyup="inputCheck"/>
+    <input class="input-box" id="input-box" :type="inputType" :disabled="validDisabled ? '' : disabled" :value="inputValue" @keyup="inputCheck"/>
+    <p v-if="hasError">{{ errorMessage }}</p>
   </div>
 </template>
 
@@ -11,7 +12,12 @@ export default {
   name: 'InputBox',
   props: {
     hasLabel: Boolean,
-    labelName: String
+    labelName: String,
+    inputType: String,
+    validDisabled: String,
+    inputValue: String,
+    hasError: Boolean,
+    errorMessage: String
   },
   methods: {
     // 부모 컴포넌트에게 input 값을 전송
@@ -32,5 +38,11 @@ export default {
 }
 .label-box {
   color: #AE5F40;
+  /* font-weight: bold; */
+}
+p{
+  margin: 0;
+  color: red;
+  font-size: 12px;
 }
 </style>
