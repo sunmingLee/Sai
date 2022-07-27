7f27179a1849c76fbde4a66fbf0984ea60868856
LeeSunMin <julia9803@naver.com>
2022-07-27 14:13:43 +0900

    Cherry-pick commit "a72ca9b6", "971617f8"

---
+++ FrontEnd/src/components/common/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button :class="buttonClass">{{ buttonText }}</button>
+  </div>
+</template>
+
+<script>
+import '@/assets/css/button.scss'
+export default {
+  props: {
+    buttonClass: String,
+    buttonText: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
