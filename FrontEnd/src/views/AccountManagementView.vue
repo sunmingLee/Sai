14b1487507a2b5c40c3372763fbff848411ad4d4
Kim Miae <miae83@konkuk.ac.kr>
2022-07-28 16:48:41 +0900

    Feat Add Withdraw Modal

--- FrontEnd/src/views/AccountManagementView.vue
+++ FrontEnd/src/views/AccountManagementView.vue
@@ -12,27 +12,45 @@
                 <p v-if="validPassword" class="valid-error">비밀번호는 영문,숫자,특수문자를 포함하여 8자 이상으로 입력하세요.</p>
                 <InputBox inputSelect="input-box" :hasLabel="true" labelName="비밀번호 확인" @inputCheck="checkPasswordConfirm"/>
                 <p v-if="validPasswordConfirm" class="valid-error">비밀번호가 일치하지 않습니다.</p>
-                <ButtonSmall buttonUsage="positive" buttonText="수정" @click="updatePassword"/>
+                <Button buttonClass="small positive" buttonText="수정" @click="updatePassword"></Button>
             </div>
         </div>
         <div class="withdrawal">
-            <p>회원탈퇴하기</p>
-            <InputBox inputSelect="input-box" :hasLabel="true" labelName="현재 비밀번호" @inputCheck="checkPassword"/>
-            <ButtonSmall buttonUsage="negative" buttonText="회원 탈퇴" @click="withdrawalMember"/>
+          <p>회원탈퇴하기</p>
+          <InputBox inputSelect="input-box" :hasLabel="true" labelName="현재 비밀번호" @inputCheck="checkPassword"/>
+          <div>
+            <Button class="btn" data-bs-toggle="modal" data-bs-target="#withdraw" buttonClass="negative" buttonText="회원탈퇴"></Button>
+          </div>
+        </div>
+        <div class="modal fade" id="withdraw" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">회원탈퇴</h5>
+              </div>
+              <div class="modal-body">
+                정말로 탈퇴하시겠습니까? 정말? 진짜로?
+              </div>
+              <div class="modal-footer">
+                <Button buttonClass="small positive" buttonText= "취소" class="btn" data-bs-dismiss="modal"></Button>
+                <Button buttonClass="small negative" buttonText="회원탈퇴" class="btn" @click="withdrawalMember" data-bs-dismiss="modal"></Button>
+              </div>
+            </div>
+          </div>
         </div>
     </div>
 </template>
 <script>
 import HeaderTitle from '@/components/common/HeaderTitle.vue'
 import InputBox from '@/components/common/InputBox.vue'
-import ButtonSmall from '@/components/common/ButtonSmall.vue'
+import Button from '@/components/common/Button.vue'
 
 export default {
   name: 'AccountManagementView',
   components: {
     HeaderTitle,
     InputBox,
-    ButtonSmall
+    Button
   },
   data () {
     return {
@@ -78,8 +96,21 @@ export default {
     },
     // 회원탈퇴
     withdrawalMember () {
+      alert('탈퇴되었습니다.')
       // 모달창
+      // 회원탈퇴 누르면 로그인창으로!
     }
   }
 }
 </script>
+
+<style>
+.modal {
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+}
+.modal-title {
+  justify-content: center
+}
+</style>
