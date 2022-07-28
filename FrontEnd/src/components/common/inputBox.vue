cdbfa7a042b0d083742c7c092a5bd92f57e33b32
LeeSunMin <julia9803@naver.com>
2022-07-28 14:38:45 +0900

    Design: Update InputBox.vue into table for align

--- FrontEnd/src/components/common/inputBox.vue
+++ FrontEnd/src/components/common/inputBox.vue
@@ -1,17 +1,27 @@
 <template>
-  <div>
-    <label class="label-box" for="input-box" v-if="hasLabel">{{ labelName }}</label>
-    <input class="input-box" id="input-box" type="text" @keyup="inputCheck"/>
+  <div class="form-wrap">
+    <table>
+      <colgroup>
+        <col width=40%>
+        <col width=50%>
+      </colgroup>
+      <tr>
+        <td><label class="label-box" for="input-box" v-if="hasLabel">{{ labelName }}</label></td>
+        <td><input class="input-box" :type="inputType" :disabled="validDisabled === ''" :value="inputValue" @keyup="inputCheck"/></td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'InputBox',
   props: {
     hasLabel: Boolean,
-    labelName: String
+    labelName: String,
+    inputType: String,
+    validDisabled: Boolean,
+    inputValue: String
   },
   methods: {
     // 부모 컴포넌트에게 input 값을 전송
@@ -22,15 +32,38 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .input-box {
-  margin-left: 5px;
-  margin-bottom: 10px;
-  width: 190px;
+  text-align: center;
+  margin-bottom: 1%;
+  width: 140px;
   border-radius: 5px;
   border: 1px solid #ae5f40;
 }
 .label-box {
   color: #AE5F40;
+}
+p{
+  margin: 0;
+  color: red;
+  font-size: 12px;
+}
+table {
+  border-collapse: collapse;
+  table-layout: fixed;
+  width: 260px;
+}
+
+td {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+.form-wrap{
+  display: inline-block;
+}
+
+.form-wrap > table > tr> td:nth-child(2) {
+  text-align: right;
 }
 </style>
