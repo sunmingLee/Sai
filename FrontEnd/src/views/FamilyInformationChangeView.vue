9bac0099d771330941d410366b80bdeda736c98c
Kim Miae <miae83@konkuk.ac.kr>
2022-08-01 17:01:55 +0900

    Feat: Add FamilyInformationChange

---
+++ FrontEnd/src/views/FamilyInformationChangeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <HeaderTitle title="가족정보수정"></HeaderTitle>
+  </div>
+</template>
+<script>
+import HeaderTitle from '@/components/common/HeaderTitle.vue';
+export default {
+  name: 'FamilyInformationChangeView',
+  components: { HeaderTitle }
+}
+</script>
+<style lang="">
+  
+</style>
