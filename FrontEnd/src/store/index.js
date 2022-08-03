9d4c9d24b13ab081a65e1c5ca245c18841c6d320
subin <5515983@naver.com>
2022-08-03 12:37:47 +0900

    Feat: Add family code copy function

--- FrontEnd/src/store/index.js
+++ FrontEnd/src/store/index.js
@@ -1,6 +1,6 @@
 import router from '@/router'
 import axios from 'axios'
-import Vue from 'vue'
+
 import Vuex from 'vuex'
 
 export default new Vuex.Store({
@@ -10,58 +10,55 @@ export default new Vuex.Store({
   getters: {
   },
   mutations: {
-    
+
   },
   actions: {
-    //아이디 찾기
-    findId({commit}, userInfo) {
-        const api_url = `http://localhost:8080/api/user/findId`
-        const params = {
-            userName: userInfo.userName,
-            email: userInfo.email
+    // 아이디 찾기
+    findId ({ commit }, userInfo) {
+      const api_url = 'http://localhost:8080/api/user/findId'
+      const params = {
+        userName: userInfo.userName,
+        email: userInfo.email
+      }
+      axios({
+        url: api_url,
+        method: 'GET',
+        params
+      }).then((res) => {
+        if (res.data.msg.indexOf('입력하신 이메일로 아이디가 전송되었습니다.') !== -1) {
+          alert(res.data.msg)
+          router.push({ name: 'home' })
+        } else {
+          alert(res.data.msg)
         }
-        axios({
-            url: api_url,
-            method: 'GET',
-            params
-        })
-        .then((res)=> {
-            if(res.data.msg.indexOf('입력하신 이메일로 아이디가 전송되었습니다.') != -1) {
-                alert(res.data.msg)
-                router.push({name : 'home'})
-            }
-            else {
-                alert(res.data.msg)
-            }
-        })
-        .catch((err)=> {
-            console.log(err)
+      })
+        .catch((err) => {
+          console.log(err)
         })
     },
-    //비밀번호 찾기
-    findPassword({commit}, userInfo) {
-        const api_url = `http://localhost:8080/api/user/findPw`
-        const params = {
-            userName: userInfo.userName,
-            userId: userInfo.userId,
-            email: userInfo.email
-        }
-        axios({
-            url: api_url,
-            method: 'GET',
-            params
+    // 비밀번호 찾기
+    findPassword ({ commit }, userInfo) {
+      const api_url = 'http://localhost:8080/api/user/findPw'
+      const params = {
+        userName: userInfo.userName,
+        userId: userInfo.userId,
+        email: userInfo.email
+      }
+      axios({
+        url: api_url,
+        method: 'GET',
+        params
+      })
+        .then((res) => {
+          if (res.data.msg.indexOf('입력하신 이메일로 임시 비밀번호가 전송되었습니다.') !== -1) {
+            alert(res.data.msg)
+            router.push({ name: 'home' })
+          } else {
+            alert(res.data.msg)
+          }
         })
-        .then((res)=> {
-            if(res.data.msg.indexOf('입력하신 이메일로 임시 비밀번호가 전송되었습니다.') != -1) {
-                alert(res.data.msg)
-                router.push({name : 'home'})
-            }
-            else {
-                alert(res.data.msg)
-            }
-        })
-        .catch((err)=> {
-            console.log(err)
+        .catch((err) => {
+          console.log(err)
         })
     }
   },
--- FrontEnd/src/views/FamilyInviteView.vue
+++ FrontEnd/src/views/FamilyInviteView.vue
@@ -1,11 +1,20 @@
 <template>
     <div class="family-wrap">
-        <HeaderTitle hasBack="true" title="가족들을 불러볼까요?"/>
-        <GuideMessage guide="회원님의 가족 코드입니다." guide2="가족들과 클로저를 함께하세요."/>
-        <div class="code-wrap">
-            <p class="fmaily-code">394 027</p>
+        <HeaderTitle hasBack=true title="가족들을 불러볼까요?"/>
+        <div class="flex">
+            <GuideMessage guide="회원님의 가족 코드입니다." guide2="가족들과 클로저를 함께하세요."/>
         </div>
-        <Button buttonText="코드복사" buttonClass="small information" />
+        <div class="flex">
+            <div class="code-wrap">
+                <p id="family-code" class="family-code">{{familyCode}}</p>
+            </div>
+        </div>
+        <div class="flex">
+            <Button buttonText="코드복사" buttonClass="small information" @click="copyCode"/>
+        </div>
+        <div class="flex">
+            <Button buttonText="메인으로" buttonClass="small positive" @click="goMain"/>
+        </div>
     </div>
 </template>
 <script>
@@ -13,20 +22,47 @@ import Button from '@/components/common/Button.vue'
 import HeaderTitle from '@/components/common/HeaderTitle.vue'
 import GuideMessage from '@/components/GuideMessage.vue'
 
+import { mapState } from 'vuex'
+
 export default {
-    name: 'FamilyInviteView',
-    components: {
-        Button,
-        HeaderTitle,
-        GuideMessage
+  name: 'FamilyInviteView',
+  components: {
+    Button,
+    HeaderTitle,
+    GuideMessage
+  },
+  methods: {
+    copyCode () {
+      const obj = document.getElementById('family-code')
+      const range = document.createRange()
+      range.selectNode(obj.childNodes[0])
+      const sel = window.getSelection()
+      sel.removeAllRanges()
+      sel.addRange(range)
+
+      document.execCommand('copy')
+      sel.removeRange(range)
+    },
+    goMain () {
+      this.$router.push({name: 'searchId'})
     }
+  },
+  computed: {
+    ...mapState(['familyCode'])
+  }
 }
 </script>
 <style lang="scss" scoped>
 .family-wrap {
+  width: 900px;
+
+  .flex{
+    display: flex;
+    justify-content: center;
     text-align: center;
+  }
 }
-.fmaily-code {
+.family-code {
     font-size: 60px;
 }
 </style>
