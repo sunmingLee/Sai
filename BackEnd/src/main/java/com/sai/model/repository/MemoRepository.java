80fe711a63cbfc6640fa3ea952fd1a4652cc555b
JDEV <xxxjinjordan@gmail.com>
2022-08-04 17:56:42 +0900

    Feat: '메모 API 구현'
    - 메모 등록
    - 팝업 메모 리스트 조회(유저별)
    - 전체 메모 리스트 조회(가족별)
    - 메모 리스트 일괄 읽음 처리(유저별)
    - 메모지 컬러 변경
    - 메모 삭제

--- BackEnd/src/main/java/com/sai/model/repository/MemoRepository.java
+++ BackEnd/src/main/java/com/sai/model/repository/MemoRepository.java
@@ -1,12 +1,14 @@
 package com.sai.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.sai.model.entity.Family;
-import com.sai.model.entity.User;
+import com.sai.model.entity.Memo;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
-	public Family findFamilyByUserId(String userId);
+public interface MemoRepository extends JpaRepository<Memo, Long>{
+	public List<Memo> findMemosByFamily(Family family);
 }
