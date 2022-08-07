6dab1eb0a7bec199f5701680c86ac208183ef02a
LeeSunMin <julia9803@naver.com>
2022-08-07 17:29:52 +0900

    Chore: tiny changes

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import AddInformationView from '../views/user/AddInformationView.vue'
 import FamilyInviteView from '@/views/FamilyInviteView.vue'
