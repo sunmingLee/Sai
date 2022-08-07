70b73642bb53cba6e681895193950cbc81e4d685
이선민 <julia9803@naver.com>
2022-08-07 18:05:38 +0900

    Revert "Merge branch 'FE-Login' into 'FE'"
    This reverts merge request !23

--- FrontEnd/src/components/login/LoginInput.vue
+++ FrontEnd/src/components/login/LoginInput.vue
@@ -1,56 +1,25 @@
 <template>
   <div>
-    <InputBox :hasLabel="true" labelName="ID" inputType="text" @inputCheck="putId"></InputBox>
-    <InputBox :hasLabel="true" labelName="PW" @inputCheck="putPassword" @keyup.enter="onLogin"></InputBox>
-    <div>
-      <Button buttonClass="small positive" buttonText="LOGIN" @click="onLogin"></Button>
+    <InputBox :hasLabel="true" labelName="ID"></InputBox>
+    <InputBox :hasLabel="true" labelName="PW"></InputBox>
+    <div align="right" style="margin-right: 70px">
+      <Button
+        id="buttonLogin"
+        buttonUsage="positive"
+        buttonText="LOGIN"
+      ></Button>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex'
 import InputBox from '@/components/common/InputBox.vue'
 import Button from '@/components/common/Button.vue'
-
-const userStore = 'userStore'
 export default {
   name: 'LoginInput',
   components: {
     InputBox,
     Button
-  },
-  data () {
-    return {
-      user: {
-        userId: '',
-        password: ''
-      }
-    }
-  },
-  computed: {
-    ...mapState(userStore, ['isLogin', 'isLoginError'])
-  },
-  methods: {
-    ...mapActions(userStore, ['login', 'getUserInfo']),
-    putId (userId) {
-      this.user.userId = userId
-    },
-    putPassword (password) {
-      this.user.password = password
-    },
-    async onLogin () {
-      this.login(this.user)
-      // await this.userConfirm(this.user)
-      // const token = sessionStorage.getItem('access-token')
-      if (this.isLogin) {
-        // console.log('isLogin')
-        this.getUserInfo(localStorage.getItem('userId'))
-        // this.notifyVue(1, '<span> <b>로그인 완료</b> </span>')
-        // this.$router.push({ name: 'about' })
-      }
-    }
-
   }
 }
 </script>
