186ca2f65d7de13adaf6390cf85188fc233495cb
LeeSunMin <julia9803@naver.com>
2022-08-09 21:20:33 +0900

    Feat: Set vuex-persistedstate

--- FrontEnd/src/store/index.js
+++ FrontEnd/src/store/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable camelcase */
 import Vuex from 'vuex'
 
+// vuex 새로고침시 상태 초기화 방지
+import createPersistedState from 'vuex-persistedstate'
+
 import familyStore from './modules/familyStore.js'
 import userStore from './modules/userStore.js'
 import boardStore from './modules/boardStore.js'
@@ -23,5 +26,12 @@ const store = new Vuex.Store({
     notificationStore,
     joinStore
   },
+  plugins: [
+    createPersistedState({
+      // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
+      // storage: sessionStorage
+    })
+  ]
+
 })
 export default store
