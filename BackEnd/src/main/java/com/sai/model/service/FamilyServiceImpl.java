5f5618bc7ebe83c5311c18a0af98a493b7caf692
JDEV <xxxjinjordan@gmail.com>
2022-08-11 18:49:23 +0900

    Fixes: "Family Apply 로직 수정"

--- BackEnd/src/main/java/com/sai/model/service/FamilyServiceImpl.java
+++ BackEnd/src/main/java/com/sai/model/service/FamilyServiceImpl.java
@@ -109,17 +109,23 @@ public class FamilyServiceImpl implements FamilyService {
 	@Override
 	public void applyFamily(InsertFamilyRegisterRequestDto insertFamilyRegisterRequestDto) {
 		FamilyRegister familyRegister = modelMapper.map(insertFamilyRegisterRequestDto, FamilyRegister.class);
+		System.out.println(familyRegister.getFamilyRegisterId());
 		Family family = familyRepository.findById(insertFamilyRegisterRequestDto.getFamilyId()).get();
+
+//		familyRegisterRepository.save(modelMapper.map(insertFamilyRegisterRequestDto, FamilyRegister.class));
+		familyRegisterRepository.saveAndFlush(familyRegister);
+		
+		familyRegister = familyRegisterRepository.findOneByUser(userRepository.findByUserId(insertFamilyRegisterRequestDto.getUserId()).get());
+		System.out.println(familyRegister.getFamilyRegisterId());
 
 		List<User> userList = family.getUsers();
 		for (User user : userList) {
+			System.out.println(familyRegister.getFamilyRegisterId());
 			CreateNotificationRequestDto cnrd = CreateNotificationRequestDto.builder()
 					.notiFromUserId(insertFamilyRegisterRequestDto.getUserId()).notiToUserId(user.getUserId())
 					.notiContent("님이 당신의 가족인가요?").notiType(NotiType.FAMILYREGISTER).notiContentId(Long.toString(familyRegister.getFamilyRegisterId())).build();
 			notiService.createNoti(cnrd);
 		}
-
-		familyRegisterRepository.save(modelMapper.map(insertFamilyRegisterRequestDto, FamilyRegister.class));
 	}
 
 	@Override
