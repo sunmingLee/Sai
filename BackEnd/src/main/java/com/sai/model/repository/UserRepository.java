1f2a046cb09a5dfcb5aefba861e10c90ee479d96
JDEV <xxxjinjordan@gmail.com>
2022-08-13 02:07:15 +0900

    Settings

--- BackEnd/src/main/java/com/sai/model/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sai.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.sai.model.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-
-}
