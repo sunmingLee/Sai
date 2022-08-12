f463f1342046cf15277377af74b49d0635275c1a
Kim Miae <miae83@konkuk.ac.kr>
2022-08-12 13:30:01 +0900

    Design: Change Memo Color

--- FrontEnd/src/components/common/BottomTap.vue
+++ FrontEnd/src/components/common/BottomTap.vue
@@ -1,16 +1,16 @@
 <template>
   <ul class="nav nav-pills nav-fill" id="pills-tab" role="tablist">
     <li class="nav-item" role="presentation">
-      <button class="nav-link active" id="feed-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" @click="moveFeed">피드</button>
+      <button class="nav-link" id="feed-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true" aria-current="page" ick="moveFeed">피드</button>
     </li>
     <li class="nav-item" role="presentation">
-      <button class="nav-link" id="refride-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false" @click="moveRefridge">냉장고</button>
+      <button class="nav-link" id="refride-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false" aria-current="page" @click="moveRefridge">냉장고</button>
     </li>
     <li class="nav-item" role="presentation">
-      <button class="nav-link" id="album-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" @click="moveAlbum">앨범</button>
+      <button class="nav-link" id="album-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" aria-current="page" @click="moveAlbum">앨범</button>
     </li>
     <li class="nav-item" role="presentation">
-      <button class="nav-link" id="calendar-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" @click="moveCalendar">달력</button>
+      <button class="nav-link" id="calendar-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false" aria-current="page" @click="moveCalendar">달력</button>
     </li>
   </ul>
 </template>
--- FrontEnd/src/views/FeedView.vue
+++ FrontEnd/src/views/FeedView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="feed-tab">
         <FeedHeader />
         <FeedContent/>
         <BottomTap></BottomTap>
