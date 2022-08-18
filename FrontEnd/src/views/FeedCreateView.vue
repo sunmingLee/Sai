b47ef80f9ccfd8c76b5e36a87c07ca4871e7b932
ssubing <5515983@naver.com>
2022-08-19 00:37:10 +0900

    Fix: FeedCreate file valid

--- FrontEnd/src/views/FeedCreateView.vue
+++ FrontEnd/src/views/FeedCreateView.vue
@@ -225,7 +225,6 @@ export default {
       srcList: [],
       fileList: [],
       imageFlag: false,
-      fileFlag: true,
     }
   },
   created () {
@@ -273,11 +272,6 @@ export default {
         // 파일 하나 선택
         const file = files[i]
         let heicFile = ''
-        if(file.name.split('.')[1] !== 'jpg' && file.name.split('.')[1] !== 'png'
-          && file.name.split('.')[1] !== 'heic' && file.name.split('.')[1] !== 'mp4') {
-          alert('지원하는 확장자가 아닙니다')
-          this.fileFlag = false
-        } else {
           // 파일의 확장자가 heic일 경우
           if (file.name.split('.')[1] === 'heic') {
             // file의 타입을 "image/jpg"로 바꾸고 이름 뒤에 확장자도 .jpg로 바꾼다
@@ -292,7 +286,6 @@ export default {
           } else {
             fileList.push(file)
           }
-        }
       }
     },
     previewFile () {
